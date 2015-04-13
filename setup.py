7482538996de6bfe9d979b696b6df9469656da5a
Jim Wyllie <jwyllie83@gmail.com>
2015-04-13 17:05:49 -0400

    Simple setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PySports',
+	version='0.1',
+	description='Python parser for sports-reference.com pages',
+	author='Jim Wyllie',
+	author_email='jwyllie83@gmail.com',
+	url='https://github.com/jwyllie83/pysports',
+	packages=['pysports'],
+)
