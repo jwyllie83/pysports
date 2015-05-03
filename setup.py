ae2b2f31dc5b7b1383c06a0434e63afad753ec76
Jim Wyllie <jwyllie83@gmail.com>
2015-05-03 11:14:28 -0400

    Bumped the version number for bugfixes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='PySports',
-	version='0.1',
+	version='0.1.1',
 	description='Python parser for sports-reference.com pages',
 	author='Jim Wyllie',
 	author_email='jwyllie83@gmail.com',
